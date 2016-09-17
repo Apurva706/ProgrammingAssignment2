f4b63a51c4ec51535e98142f468cd23c98dff0a6
Apurva706 <priyadarshi.a.purv@gmail.com>
2016-09-18 01:26:10 +0530

    Submit version to hub

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inverse is a computationally expensive procedure
+## For efficient computation, making a function that can 
+## take cached inverse value, if available
 
-## Write a short comment describing this function
+## First function computes the inverse of matrix and outputs
+## a list which has inverse and the environment values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(mean) m <<- solve(x)
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+  
 }
 
 
-## Write a short comment describing this function
+
+## CacheSolve tries to find if a cached value exists
+## and outputs value from cache if available
+## thus improving efficiency
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
+  
 }
+
